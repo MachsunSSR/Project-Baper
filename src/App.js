660b29f0ed085fe7bea9c75a408a3e2741a2a7ca
Machsun SSR <safirmachsun@gmail.com>
2020-05-25 22:21:32 +0700

    Midnight Update

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import Jam from "./Jam";
 
 function App() {
   const [clickCounter, setClickCounter] = useState(0);
-  const [ada, setAda] = useState(false);
 
   let greet = "";
   const name = "Nadine";
@@ -24,12 +23,15 @@ function App() {
     greet = `Good Night, ${name}.`;
   } else if (hour >= 17 && hour <= 18) {
     greet = `Good Evening, ${name}.`;
+  } else if (hour >= 0 && hour <= 3) {
+    isDay = false;
+    greet = `Good Night, ${name}.`;
   }
 
   const ReactTypingEffectDemo = () => {
     return (
       <ReactTypingEffect
-        eraseDelay={10000000000}
+        eraseDelay={10000}
         text={greet}
         speed={100}
         typingDelay={1500}
@@ -40,7 +42,6 @@ function App() {
   const clickFunction = () => {
     console.log(clickCounter);
     setClickCounter(clickCounter + 1);
-    setAda(clickCounter > 0 ? true : false);
   };
 
   return (
@@ -55,10 +56,16 @@ function App() {
       <div className="content">
         <Jam />
         <h1>{ReactTypingEffectDemo()}</h1>
-        <h2 className={clickCounter > 1 ? "thanks" : "nope"}>
-          Makasih ya kopinya :)
-        </h2>
-        <h3 className={clickCounter > 2 ? "thanks" : "nope"}>Kuy Mabar</h3>
+        <div
+          className="addision"
+          style={{
+            display: hour >= 1 && hour <= 3 ? "inline" : "none",
+          }}
+        >
+          <h2 className={clickCounter > 1 ? "thanks" : "nope"}>
+            {hour >= 1 && hour <= 3 ? "Ngapain malem malem kesini?" : ""}
+          </h2>
+        </div>
       </div>
     </div>
   );
